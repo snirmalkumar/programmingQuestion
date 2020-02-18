4c2b3c7082b16aa58923269d09a92ffc50d062a0
Nirmalkumar Selvaraj <snirmalkumar1990@gmail.com>
2020-02-18 12:24:35 +0530

    feat: Added associatedtype example

--- RemoveDuplicate/MyPlayground.playground/Contents.swift
+++ RemoveDuplicate/MyPlayground.playground/Contents.swift
@@ -16,3 +16,63 @@ func removeDuplicatedCharacters(str: String) {
 }
 removeDuplicatedCharacters(str: str)
 removeDuplicatedCharacters(str: str1)
+
+/*
+ * An associated type gives a placeholder name to a type that is used as part of the protocol.
+ * The actual type to use for that associated type isn’t specified until the protocol is adopted.
+ * Associated types are specified with the associatedtype keyword.
+ */
+
+/*
+ * Example
+ */
+
+protocol ItemsBasket {
+    associatedtype ItemDataType
+    var itemsList: [ItemDataType] {get set}
+    mutating func addItem(id: ItemDataType)
+    func getAllBasketItems()-> [ItemDataType]
+}
+
+struct NumericProductItemsBasket: ItemsBasket {
+    typealias ItemDataType = UInt
+    var itemsList = [UInt]()
+
+    mutating func addItem(id: UInt) {
+        itemsList.append(id)
+    }
+
+    func getAllBasketItems() -> [UInt] {
+        return itemsList
+    }
+
+}
+
+struct AlphaProductItemsBasket: ItemsBasket {
+    typealias ItemDataType = String
+    var itemsList = [String]()
+
+    mutating func addItem(id: String) {
+        itemsList.append(id)
+    }
+
+    func getAllBasketItems() -> [String] {
+        return itemsList
+    }
+
+}
+
+#if true
+var numericProducts = NumericProductItemsBasket()
+
+numericProducts.addItem(id: 100)
+numericProducts.addItem(id: 101)
+print(numericProducts.getAllBasketItems())
+
+var alphaProducts = AlphaProductItemsBasket()
+
+alphaProducts.addItem(id: "ABC100")
+alphaProducts.addItem(id: "ABC101")
+print(alphaProducts.getAllBasketItems())
+#endif
+
